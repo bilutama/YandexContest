40bd3f867e98c725be6255afcb90b7e74b6e5f09
Maxim Biluta <biluta.m.a@gmail.com>
2023-11-27 02:42:23 +0100

    Contest

---
+++ src/Contest/MergingSequences.java
@@ -0,0 +1,68 @@
+package Contest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class MergingSequences {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        StringTokenizer tokenizer = new StringTokenizer(str);
+        int n = Integer.parseInt(tokenizer.nextToken());
+
+        System.out.println(getElement(n));
+    }
+
+    private static long getElement(int n) {
+        if (n == 1) {
+            return 1;
+        }
+
+        int index = 0;
+        long a;
+        long b;
+
+        long m = 1;
+
+        for (long j = 1L; j <= n + 1; j = j + 1) {
+            b = j * j * j;
+
+            for (long i = m; i <= n + 1; i = i + 1) {
+                m += 1;
+
+                a = i * i;
+
+                if (a == b) {
+                    continue;
+                }
+
+                if (a < b) {
+                    index = index + 1;
+                    if (index == n) {
+                        return a;
+                    }
+
+                    continue;
+                }
+
+                index = index + 1;
+                if (index == n) {
+                    return b;
+                }
+
+                index = index + 1;
+                if (index == n) {
+                    return a;
+                }
+
+                break;
+            }
+
+        }
+
+        return 0;
+    }
+}
---
+++ src/Contest/MirrorZFunction.java
@@ -0,0 +1,24 @@
+package Contest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class MirrorZFunction {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        StringTokenizer tokenizer = new StringTokenizer(s);
+        int n = Integer.parseInt(tokenizer.nextToken());
+
+        s = br.readLine();
+        System.out.println();
+    }
+
+    public static int getValueOfZ(String s, int chatIndex) {
+
+        return 0;
+    }
+}
