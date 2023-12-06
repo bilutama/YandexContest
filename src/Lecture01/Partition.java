8066b5dbfeff2b5431e4d3d6147a1b6d1bb5d212
Maxim Biluta <biluta.m.a@gmail.com>
2023-12-06 14:42:26 +0100

    Partition

--- src/Lecture01/Partition.java
+++ src/Lecture01/Partition.java
@@ -1,7 +1,43 @@
 package Lecture01;
 
-public class Partition {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.function.Predicate;
 
+public class Partition {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+        int size = Integer.parseInt(tokenizer.nextToken());
+
+        int[] e = new int[size];
+        tokenizer = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < size; i++) {
+            e[i] = Integer.parseInt(tokenizer.nextToken());
+        }
+
+        tokenizer = new StringTokenizer(br.readLine());
+        int ref = Integer.parseInt(tokenizer.nextToken());
+
+        int counter = partition(e, 0, e.length - 1, x -> x < ref);
+
+        System.out.println(counter);
+        System.out.println(size - counter);
+    }
+
+    public static int partition(int[] array, int start, int end, Predicate<Integer> predicate) {
+        int counter = 0;
+
+        for (int i : array) {
+            if (predicate.test(i)) {
+                ++counter;
+            }
+        }
+
+        return counter;
     }
 }
